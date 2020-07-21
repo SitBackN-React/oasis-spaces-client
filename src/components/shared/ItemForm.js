a184fbc020b1a9487f047c928ea9355e051f1743
Christine <huangc909@gmail.com>
2020-07-20 22:46:03 -0400

    Changed display for forms

--- src/components/shared/ItemForm.js
+++ src/components/shared/ItemForm.js
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom'
 
 const ItemForm = ({ item, handleSubmit, handleChange, cancelPath }) => (
   <form onSubmit={handleSubmit}>
-    <label>Item Name</label>
-    <input
-      placeholder="Example: Go get newspaper"
-      value={item.name}
-      name="name"
-      onChange={handleChange}
-    />
-
-    <label>Note</label>
-    <input
-      placeholder="(Optional) Example: Get after 9am"
-      value={item.note}
-      name="note"
-      onChange={handleChange}
-    />
-
+    <div>
+      <label>Item Name</label>
+      <input
+        placeholder="Example: Go get newspaper"
+        value={item.name}
+        name="name"
+        onChange={handleChange}
+      />
+    </div>
+    <div>
+      <label>Note</label>
+      <input
+        placeholder="(Optional) Example: Get after 9am"
+        value={item.note}
+        name="note"
+        onChange={handleChange}
+      />
+    </div>
     <button type="submit">Submit</button>
     <Link to={cancelPath}>
       <button>Cancel</button>
