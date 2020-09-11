ec58ceaafbbb6082abeba4b156a86d726e09e015
ItsAllDari <itsalldari@gmail.com>
2020-09-11 16:21:05 -0400

    Updated header to remove home page link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,22 +18,21 @@ const unauthenticatedOptions = (
   </Fragment>
 )
 
-const alwaysOptions = (
-  <Fragment>
-    <Nav.Link to="/"></Nav.Link>
-  </Fragment>
-)
+// const alwaysOptions = (
+//   <Fragment>
+//     <Nav.Link to="/"></Nav.Link>
+//   </Fragment>
+// )
 
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
-    <Navbar.Brand href="#">
+    <Navbar.Brand>
       <img src="https://media.git.generalassemb.ly/user/28180/files/56a98e80-cab7-11ea-850a-52fae3bc0849" alt="Palm Tree"/> Oasis Spaces
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
         { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-        { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
